f384360aba06b402240031e832821af541080b8f
bax082024 <bax082024@gmail.com>
2025-02-16 12:57:30 +0100

    added user menu

--- Program.cs
+++ Program.cs
@@ -5,7 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        while (true)
+        {
+            
+            Console.WriteLine("=== TXT to CSV / CSV to TXT Converter ===");
+            Console.WriteLine("1. Convert TXT to CSV");
+            Console.WriteLine("2. Convert CSV to TXT");
+            Console.WriteLine("3. Exit");
+            Console.Write("Choose an option (1-3): ");
 
+        }
 
     }
 
